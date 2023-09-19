aee4f3e6cfbc8be9b2d6cfeb2d036a315d294b56
Om Narayan Trivedi <omtrivedi.7824@gmail.com>
2023-09-19 14:06:09 +0530

    Easy

---
+++ FirstSetBit.cpp
@@ -0,0 +1,47 @@
+//Find location of first set bit
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        int count = 0;
+        while(n != 0)
+        {
+            count++;
+            if(n & 1 == 1){
+            return count;}
+            n = n >> 1;
+        }
+        return count;
+        // Your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
